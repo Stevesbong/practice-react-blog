e1598f0ad8c09ef7e95d199bc847a2c42787adda
Steve Park <ghkfkd0323@gmail.com>
2022-03-24 01:35:14 -0700

    add blog details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Navbar from './Navbar'
 import Home from './Home'
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
+// json server package
+// npx json-server --watch data/db.json --port 8000
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,14 @@
-const BlogList = ({blogs, handleDelete}) => {
+import { Link } from "react-router-dom";
+
+const BlogList = ({blogs}) => {
     return (
         <div className="blog-list">
             {blogs.map( (blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                    <button onClick={ () => handleDelete(blog.id) }>delete blog</button>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>
